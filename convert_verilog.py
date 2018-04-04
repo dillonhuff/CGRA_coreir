72356209540115a6458d90ac739d685dfbb95c28
Dillon Huff <dillonhuff@gmail.com>
2018-04-03 23:01:09 -0700

    Slightly better now

--- convert_verilog.py
+++ convert_verilog.py
@@ -2,15 +2,6 @@ import os
 
 verilog_dir = '/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/'
 
-# sv_files_old = """/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/cfg_and_dbg_unq1.sv \\ """
-
-# sv_files_old = """/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/cfg_ifc_unq1.sv \\ """
-
-# sv_files = """/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/flop_unq1.sv \\ """
-
-# sv_files = """/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/flop_unq2.sv \\
-# /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/flop_unq3.sv \\ """
-
 sv_files = """./global_controller_unq1.sv \\
 /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/test_cmpr.sv \\
 /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/test_debug_reg.sv \\
@@ -47,14 +38,50 @@ v_files = """/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/t
 /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/sb_unq2.v \\
 /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/sb_unq3.v \\
 /Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/sram_512w_16b.v \\
-/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/top.v \\ """
+/Users/dillon/VerilogWorkspace/CGRAGenerator/hardware/generator_z/top/genesis_verif/top.v"""
 
 files_str = sv_files + ' ' + v_files
-# + verilog_dir + '*.v'
+
+def remove_macro_def(macro_name, fstr):
+    lines = fstr.splitlines()
+
+    final_str = ''
+    macro_len = len(macro_name)
+    define_len = len('define')
+    for line in lines:
+        if len(line) >= (macro_len + define_len + 1) and line[0] == '`':
+            print 'Found macro def = ', line
+            base = 1 + define_len + 1
+            print line[base : base + macro_len]
+
+            if line[base : base + macro_len] == macro_name:
+                final_str += line[0 : base + macro_len]
+            else:
+                final_str += line + '\n'
+        else:
+            final_str += line + '\n'
+
+    return final_str
+
+# Preprocess away xasserts from files
+files_to_proc = ['input_sr_unq1.v', 'output_sr_unq1.v', 'memory_core_unq1.v']
+for file in files_to_proc:
+    file_name = verilog_dir + file
+    f = open(file_name, 'r')
+    fstr = f.read();
+    f.close()
+    
+    new_fstr = remove_macro_def('xassert', fstr)
+
+    f = open(file_name, 'w')
+    f.write(new_fstr)
+    f.close()
+
 
 yosys_to_coreir_cmd_str = 'yosys -p "hierarchy; proc; memory -nomap; pmuxtree; to_coreir" -m /Users/dillon/CppWorkspace/VerilogToCoreIR/to_coreir.so ' + files_str
 
 #verilog_dir + '*.sv ' + verilog_dir + '*.v'
+print 'Process command = '
 print yosys_to_coreir_cmd_str
 
 os.system(yosys_to_coreir_cmd_str)
